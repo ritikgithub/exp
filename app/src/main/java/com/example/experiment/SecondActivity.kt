0d0f22f4918b85f49dd33f0eb101f9ececabf61c
Ritik Agarwal <hrithik1998@gmail.com>
2021-11-09 10:17:35 +0530

    second Commit

--- app/src/main/java/com/example/experiment/SecondActivity.kt
+++ app/src/main/java/com/example/experiment/SecondActivity.kt
@@ -2,10 +2,12 @@ package com.example.experiment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        Log.d("ritik","Second Activity OnCreate")
     }
 }
