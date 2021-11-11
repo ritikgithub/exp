cd63344f9a358d30bedb22629403d851dacfebd6
ritikgithub <44520380+ritikgithub@users.noreply.github.com>
2021-11-11 10:36:13 +0530

    Update FirstFragment.kt

--- app/src/main/java/com/example/experiment/FirstFragment.kt
+++ app/src/main/java/com/example/experiment/FirstFragment.kt
@@ -11,7 +11,7 @@ import android.widget.Button
 import android.widget.TextView
 
 class FirstFragment : Fragment() {
-
+//first Fragment remote
     override fun onAttach(context: Context) {
         super.onAttach(context)
         Log.d("ritik","Fragmentt 1 onAttach")
